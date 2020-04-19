08d783be92a92e543962dc494f2d2b895ba8424f
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 20:32:50 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {
-    "name" => "Nelson Ramirez"
+    "name" => "Nelson Ramirez",
     "age" => 28
   }
   return your_hash
@@ -28,7 +28,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  return shipping_manifest["oil paintings"]
 end
 
 def adding
