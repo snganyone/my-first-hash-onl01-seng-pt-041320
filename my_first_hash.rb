6bd25c4952f95a144504e3971cbe49f2a5592cd2
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-19 20:31:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {
-    "name" => "Nelson Ramirez"
+    "name" => "Nelson Ramirez",
     "age" => 28
   }
   return your_hash
@@ -48,3 +48,4 @@ def adding
   # return the shipping_manifest hash below
   return shipping_manifest
 end
+ 
